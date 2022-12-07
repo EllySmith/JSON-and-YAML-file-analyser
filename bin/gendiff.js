4e2efe89713e87fb4cb9198ec1cfbcc849572ce1
Alexandra Adelson <adelson.alexandra@gmail.com>
2022-12-07 11:48:28 +0100

    first step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+const { program } = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format')
-  .option('-h, --help');
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
