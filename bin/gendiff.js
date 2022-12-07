a4a7fed2a7c3196d370fdb44b941ab12909e895a
Alexandra Adelson <adelson.alexandra@gmail.com>
2022-12-07 09:54:47 +0100

    gendiff file

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .option('-h, --help');
+
+program.parse();
