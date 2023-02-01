84633891474597633b59f94ef329faedc7c8b81a
Alexandra Adelson <adelson.alexandra@gmail.com>
2023-02-01 11:50:13 +0100

    node files

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ install:
 
 gendiff:
 	node gendiff.js
+
+lint:
+	npx eslint
--- src/compare.js
+++ src/compare.js
@@ -5,7 +5,7 @@ export default function compareObjects(obj1, obj2) {
   const linesSecondObject = Object.keys(obj2);
   for (const key of linesFirstObject) {
     if (!linesSecondObject.includes(key)) {
-     console.log(`- ${key}: ${obj1[key]}`);
+      console.log(`- ${key}: ${obj1[key]}`);
     }
     if (linesSecondObject.includes(key) && Object.is(obj1[key], obj2[key])) {
       console.log(`  ${key}: ${obj1[key]}`);
@@ -16,8 +16,8 @@ export default function compareObjects(obj1, obj2) {
     }
   }
   for (const key of linesSecondObject) {
-     if (!linesFirstObject.includes(key)) {
-          console.log(`+ ${key}: ${obj2[key]}`)
-     }
+    if (!linesFirstObject.includes(key)) {
+      console.log(`+ ${key}: ${obj2[key]}`);
+    }
   }
-};
+}
