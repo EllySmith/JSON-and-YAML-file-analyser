30c2c563e746197d29e15e5119e10c9df32e7677
Alexandra Adelson <adelson.alexandra@gmail.com>
2023-03-20 11:17:59 +0100

    yaml fix

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import applyFormat from './formatters/index.js';
 
 import * as path from 'path';
 
-import filePath from './filePath.js';
-
 import compareObjects from './compare.js';
+
+import parseFile from './filePath.js';
 
 const getPath = (filename) => path.resolve(process.cwd(), filename);
 
@@ -18,10 +18,14 @@ const getFileFormat = (filename) => path.extname(filename).slice(1);
 
 const parsPath = (filepath1, filepath2, formatName = 'stylish') => {
   const path1 = getPath(filepath1);
-  const data1 = JSON.parse(readFile(path1, 'utf8'));
+  console.log(path1);
+  const data1 = parseFile(readFile(path1, 'utf8'), getFileFormat(filepath1));
+console.log(data1);
 
   const path2 = getPath(filepath2);
-  const data2 = JSON.parse(readFile(path2, 'utf8'));
+  console.log(path2);
+  const data2 = parseFile(readFile(path2, 'utf8'), getFileFormat(filepath2));
+  console.log(data2);
 
   const resultString = compareObjects(data1, data2);
   const formattedResult = applyFormat(resultString, formatName);
@@ -31,5 +35,5 @@ const parsPath = (filepath1, filepath2, formatName = 'stylish') => {
 export default parsPath;
 
 export {
-  getPath, readFile, getFileFormat,
+  getPath, readFile, getFileFormat, parsPath,
 };
