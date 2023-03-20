67ebed3d29d24b2136c0b5ba787556c81be99bb2
Alexandra Adelson <adelson.alexandra@gmail.com>
2023-03-20 12:38:58 +0100

    parser fix

--- src/parsFile.js
+++ src/parsFile.js
@@ -1,8 +1,19 @@
 import yaml from 'js-yaml';
 
 const parseFile = (data, format) => {
-  const path = { yml: yaml.load, yaml: yaml.load, json: JSON.parse };
-  return path[format](data).toString();
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+
+    case 'yaml':
+      return yaml.load(data);
+
+    case 'yml':
+      return yaml.load(data);
+
+    default:
+      throw new Error('Error format');
+  }
 };
 
 export default parseFile;
